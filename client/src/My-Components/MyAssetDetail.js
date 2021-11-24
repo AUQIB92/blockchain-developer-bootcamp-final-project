abe630fcf5e83ca21c9a2c6e95785b32d7264f55
auqib92 <auqib92@gmail.com>
2021-11-24 20:17:02 +0530

    Removed typos

--- client/src/My-Components/MyAssetDetail.js
+++ client/src/My-Components/MyAssetDetail.js
@@ -63,11 +63,9 @@ function MyAssetDetail(props) {
                 <h5 className="card-title">AssetId: {props.assetID}</h5>
                 <div className="card-text d-flex flex-column m-1 mb-3">
                     <span>Type: {props.type}</span>
-<<<<<<< HEAD
                     {!enableSale && props.avlToBuy && <span>Price:{ ethers.utils.formatEther(props.assetPrice).toString()} ETH</span>}
-=======
+
                     {!enableSale && props.avlToBuy && <span>Price: {ethers.utils.formatEther(props.assetPrice).toString()} ETH</span>}
->>>>>>> 50b0d4f38d555dd47b9048925a472cd69162dd05
                     {enableSale && <input type="string" className="form-control" id="Price" onChange={e => setAssetPrice((e.target.value))} step="any" placeholder="Price"  />}
                 </div>
                 {!enableSale && !props.avlToBuy && <button className="btn btn-primary m-1" onClick={() => { setEnableSale(true) }}>Set Price</button>}
