b2a77d9683fb223aa81ee4954863b7fcb548d400
auqib92 <auqib92@gmail.com>
2021-11-27 08:18:29 +0530

    config changes

--- truffle-config.js
+++ truffle-config.js
@@ -11,17 +11,12 @@ module.exports = {
   
      
     networks: {
-      // Useful for testing. The `development` name is special - truffle uses it by default
-      // if it's defined here and no other network is specified at the command line.
-      // You should run a client (like ganache-cli, geth or parity) in a separate terminal
-      // tab if you use this network and you must also set the `host`, `port` and `network_id`
-      // options below to some value.
-      //
+      
   
-          develop: {
+          development: {
             host: "127.0.0.1",
-            port: 8545,
-            network_id: "*" // Match any network id
+            port: 7545,
+            network_id: "5777" // Match any network id
           },
           ropsten: {
             networkCheckTimeout: 10000,
