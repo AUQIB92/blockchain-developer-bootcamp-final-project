1f78ea5cbc01fc50e8f3338ef1f8accc56514896
auqib92 <auqib92@gmail.com>
2021-11-27 19:06:54 +0530

    gitignore

--- truffle-config.js
+++ truffle-config.js
@@ -15,8 +15,8 @@ module.exports = {
   
           development: {
             host: "127.0.0.1",
-            port: 7545,
-            network_id: "5777" // Match any network id
+            port: 8545,
+            network_id: "*" // Match any network id
           },
           ropsten: {
             networkCheckTimeout: 10000,
